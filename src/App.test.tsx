80f50389430b3ac38bcd42abd528b818b8a3e7db
Michael Taiwo <22432591+Taiwonator@users.noreply.github.com>
2021-08-27 09:42:49 +0100

    added favicon + building up project structure

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders initial page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello there/i);
   expect(linkElement).toBeInTheDocument();
 });
