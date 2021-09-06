ac0c1060db5abb22ea3d174e736124440676bada
Michael <22432591+Taiwonator@users.noreply.github.com>
2021-09-06 22:32:22 +0100

    attempt: fixing github PR labeling workflow

--- src/stories/header/index.tsx
+++ src/stories/header/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 type HeaderProps = {
-<<<<<<< HEAD
     text: string, 
     Tag?: keyof JSX.IntrinsicElements
 }
@@ -12,16 +11,6 @@ const Header: React.FC<HeaderProps> = props => {
     let TagName = Tag ? Tag : 'h1'
 
     return <TagName>{ text }</TagName>
-=======
-    Tag: keyof JSX.IntrinsicElements, 
-    text: string
-}
-
-const Header: React.FC<HeaderProps> = props => {
-    const { Tag, text } = props
-
-    return <Tag>{ text }</Tag>
->>>>>>> c11ced9a2ec750fc773c5b67ab5d91ea94c73b38
 }
 
 export default Header
