167526c5b078ec9ad8486659aaf1e5a3ac271379
Michael Taiwo <22432591+Taiwonator@users.noreply.github.com>
2021-09-12 09:46:06 +0100

    feat: adding to flex-wrapper component

--- src/wrappers/FlexWrapper.tsx
+++ src/wrappers/FlexWrapper.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import './FlexWrapper.scss'
+import classNames from 'classnames'
 
 interface FlexWrapperProps {
     children: React.ReactNode, 
     name: string, 
-    Tag?: keyof JSX.IntrinsicElements
+    Tag?: keyof JSX.IntrinsicElements, 
+    page?: boolean,
+    center?: boolean,
 }
 
 const FlexWrapper: React.FC<FlexWrapperProps> = props => {
-    const { children, name, Tag } = props
+    const { children, name, Tag, page, center } = props
 
     let TagName = Tag ? Tag : 'div'
- 
+    let otherClassNames = classNames(
+        { 'flex-wrapper--page': page },
+        { 'flex-wrapper--center': center }
+    )
+
     return (
-        <TagName className={`flex-wrapper flex-wrapper--center ${name}`}>
+        <TagName className={`flex-wrapper ${name} ${otherClassNames}`}>
             {children}
         </TagName>
     )
